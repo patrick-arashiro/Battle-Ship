40698506bd945b4d059200b13a7b9e9e860df1a2
Patrick Arashiro <patrickaa@al.insper.edu.br>
2023-05-03 15:07:35 -0300

    segundo codigo

--- ep2.py
+++ ep2.py
@@ -14,3 +14,21 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             posicao.append(coluna+i)
             lista.append(posicao)
     return lista
+
+def preenche_frota(frota, navio, linha, coluna,orientacao, tamanho):
+    lista = []
+    if orientacao == 'vertical':
+        for i in range(0, tamanho):
+            lista.append([linha + i, coluna])
+
+    if orientacao == 'horizontal':
+        for i in range(0, tamanho):
+            lista.append([linha, coluna + i])
+
+    if navio in frota and lista != []:
+        frota[navio] += [lista]
+
+    if navio not in frota and lista != []:
+        frota[navio] = [lista]
+
+    return frota
